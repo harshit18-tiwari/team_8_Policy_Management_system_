bf9a69ae8ef78dcbf837dc7ab1e50a97eb4667fa
harshit18-tiwari <harshitoff18@gmail.com>
2025-12-04 14:22:21 +0530

    changes in policies

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const router = express.Router();
-const productController = require('../controllers/productController');
+    const express = require('express');
+    const router = express.Router();
+    const productController = require('../controllers/productController');
 
-router.get('/', productController.listProducts);
+    router.get('/', productController.listProducts);
 
-module.exports = router;
+    module.exports = router;
